8bd12bab710f55827df7214845a33f5e6dd95299
unknown <pedrosv6@gmail.com>
2014-09-29 00:38:47 -0300

    paddle movements scripts

---
+++ Assets/Scripts/PaddleMoviment.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleMoviment : MonoBehaviour {
+
+
+	public int Speed = 1;
+	public KeyCode upKey;
+	public KeyCode downKey;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var x = transform.position.x;
+		var y = transform.position.y;
+		var z = transform.position.z;
+
+		var newY = y;
+
+		if (Input.GetKey (upKey)) {
+			newY = y + Speed * Time.deltaTime;
+
+		} else if(Input.GetKey (downKey)){
+			newY = y - Speed * Time.deltaTime;
+		}
+
+		transform.position = new Vector3(x, newY, z);
+	}
+}
