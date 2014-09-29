603c64375843394fa85a2de7ede7cd7b484c59f0
Nicholas Alexander <nickmaclewy222@gmail.com>
2014-09-29 11:59:09 -0300

    Changing top and bot level coliders position Correcting bug with ball velocity (The ball has a minimum velocity threshold now)
    Signed-off-by: Nicholas Alexander <nickmaclewy222@gmail.com>

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -3,17 +3,31 @@ using System.Collections;
 
 public class BallMovement : MonoBehaviour
 {
-	public Vector2 force = new Vector2(100,100);
+    public Vector2 force = new Vector2(100, 100);
+    public float minVelocity = 10f;
 
-	// Use this for initialization
-	void Start ()
-	{
-		rigidbody2D.AddForce(force);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D.AddForce(force);
+        if (minVelocity > force.x)
+        {
+            minVelocity = force.x;
+        }
+        else if (minVelocity > force.y)
+        {
+            minVelocity = force.y;
+        }
+    }
 
-	// Update is called once per frame
-	void Update ()
-	{
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Current Veloc: " + rigidbody2D.velocity.magnitude);
+        if (rigidbody2D.velocity.magnitude < minVelocity)
+        {
+            rigidbody2D.AddForce(new Vector2(rigidbody2D.velocity.x + force.x / 2, rigidbody2D.velocity.y + force.y / 2));
+            Debug.Log("Changed Veloc: " + rigidbody2D.velocity.magnitude);
+        }
+    }
 }
