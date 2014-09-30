be9d542112970bb502d8da06bbd01ad4a537d0a1
Nicholas Alexander <nickmaclewy222@gmail.com>
2014-09-29 22:48:58 -0300

    Changing Ball color on collision with paddles
    Signed-off-by: Nicholas Alexander <nickmaclewy222@gmail.com>

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,6 +5,7 @@ public class BallMovement : MonoBehaviour
 {
     public Vector2 force = new Vector2(100, 100);
     public float minVelocity = 10f;
+    public Color collisionColor = new Color(1.0f, 0.6f, 0.6f);
 
     // Use this for initialization
     void Start()
@@ -23,11 +24,24 @@ public class BallMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Current Veloc: " + rigidbody2D.velocity.magnitude);
         if (rigidbody2D.velocity.magnitude < minVelocity)
         {
             rigidbody2D.AddForce(new Vector2(rigidbody2D.velocity.x + force.x / 2, rigidbody2D.velocity.y + force.y / 2));
-            Debug.Log("Changed Veloc: " + rigidbody2D.velocity.magnitude);
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        if (collisionInfo.collider.gameObject.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().color = collisionColor;
+            Invoke("CancelColor", 1.0f);
+        }
+    }
+
+    void CancelColor()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f);
+    }
+    
 }
