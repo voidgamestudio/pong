ffe253e2b42b88228edaf80367a66b1d1811f17f
wormangel <wormangel@gmail.com>
2014-10-01 04:11:20 -0300

    Paddle influence on the direction of the ball

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -20,6 +20,27 @@ public class BallMovement : MonoBehaviour
         if (collisionInfo.collider.gameObject.tag == "Player")
         {
             GetComponent<SpriteRenderer>().color = collisionColor;
+
+			// Calculate new direction of the ball
+			// Calculate hit Factor
+			float y = hitFactor(transform.position, collisionInfo.transform.position, ((BoxCollider2D)collisionInfo.collider).size.y);
+
+			// Calculate the direction (left or right)
+			float x = 1;
+			if (collisionInfo.collider.gameObject.name == "GreenPaddle") 
+			{
+				x = 1;
+			} else if (collisionInfo.collider.gameObject.name == "RedPaddle") 
+			{
+				x = -1;
+			}
+
+			// Calculate direction, set length to 1
+			Vector2 direction = new Vector2(x, y).normalized;
+			
+			// Set Velocity with direction * speed
+			rigidbody2D.velocity = direction * speed;
+
             Invoke("CancelColor", 1.0f);
         } else if (collisionInfo.collider.gameObject.name == "LeftBoundary") 
 		{
@@ -34,5 +55,15 @@ public class BallMovement : MonoBehaviour
         GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f);
     }
 
+	private float hitFactor(Vector2 ballPos, Vector2 racketPos,
+	                        float racketHeight) {
+		// ascii art:
+		// ||  1 <- at the top of the racket
+		// ||
+		// ||  0 <- at the middle of the racket
+		// ||
+		// || -1 <- at the bottom of the racket
+		return (ballPos.y - racketPos.y) / racketHeight;
+	}
     
 }
--- Assets/Scripts/PaddleMoviment.cs
+++ Assets/Scripts/PaddleMoviment.cs
@@ -15,20 +15,11 @@ public class PaddleMoviment : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
-		var x = transform.position.x;
-		var y = transform.position.y;
-		var z = transform.position.z;
-
-		var newY = y;
-
 		if (Input.GetKey (upKey)) {
 			transform.Translate(new Vector2(0.0f, 0.1f));
 
 		} else if(Input.GetKey (downKey)){
 			transform.Translate(new Vector2(0.0f, -0.1f));
 		}
-
-	 	//transform.position = new Vector3(x, newY, z);
 	}
 }
